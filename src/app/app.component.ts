434e39763f20972f7a8a8ae4456b4eb94f2187bf
Rishabh <rishabhmalhotra091@gmail.com>
2020-04-18 15:52:44 +0530

    who i am completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,118 +8,50 @@ import { Component, OnInit } from "@angular/core";
 export class AppComponent implements OnInit {
   skills = "technical";
   className = "custom-bg";
+  isTechnical=true;
 
-  technicalSkills1 = [
+  cards=[
     {
-      name: "java",
-      level: 90
+      image: "../assets/letter-blocks-247819.jpg",
+      heading: "Fast Learner",
+      desc: "Wisdom is not a product of schooling but of the lifelong attempt to acquire it",
+      owner: "- Mahatma Gandhi"
     },
     {
-      name: "c",
-      level: 50
+      image: "../assets/man-in-suit.jpg",
+      heading: "Receptive",
+      desc: "I am willing to sit down with anybody and have constructive criticism",
+      owner: "- Charles Barkley"
     },
     {
-      name: "c++",
-      level: 50
+      image: "../assets/chessboard-game.jpg",
+      heading: "Decision Maker",
+      desc: "Whenever you see a successful business, someone once made a courageous decision.",
+      owner: "- Peter F. Drucker"
     },
     {
-      name: "javascript",
-      level: 75
+      image: "../assets/comm.jpg",
+      heading: "Good Listener",
+      desc: "The most important thing in communication is to hear what isn’t being said.",
+      owner: "- Peter F. Drucker"
     },
     {
-      name: "typescript",
-      level: 75
+      image: "../assets/happy.jpg",
+      heading: "Emotional Intelligent",
+      desc: "I don't want to be at the mercy of my emotions. I want to use them, to enjoy them.",
+      owner: "- Oscar Wilde"
     },
     {
-      name: "angular",
-      level: 75
-    },
-    {
-      name: "css",
-      level: 90
-    },
-    {
-      name: "html",
-      level: 90
-    }
-  ];
-  technicalSkills2 = [
-    {
-      name: "bootstrap",
-      level: 90
-    },
-    {
-      name: "springboot",
-      level: 80
-    },
-    {
-      name: "hibernate",
-      level: 70
-    },
-    {
-      name: "native android",
-      level: 25
-    },
-    {
-      name: "electron",
-      level: 25
-    },
-    {
-      name: "oracle",
-      level: 50
-    },
-    {
-      name: "mysql",
-      level: 90
-    },
-    {
-      name: "mongodb",
-      level: 25
+      image: "../assets/curious.jpg",
+      heading: "Curious",
+      desc: "I have no special talents. I am only passionately curious.",
+      owner: "- Albert Einstein"
     }
   ];
 
   ngOnInit() {
-    window.addEventListener("load", () => {
-      if (this.checkTech(this.skills)) {
-        this.displaySkills();
-      }
-    });
   }
 
-  checkTech(skill) {
-    if (skill == "technical") {
-      return true;
-    }
-
-    return false;
-  }
-
-  displaySkills() {
-    for (let i = 0; i < this.technicalSkills1.length; i++) {
-      this.getSkills(
-        this.technicalSkills1[i].name,
-        this.technicalSkills1[i].level
-      );
-      this.getSkills(
-        this.technicalSkills2[i].name,
-        this.technicalSkills2[i].level
-      );
-    }
-  }
-
-  getSkills(id, level) {
-    let bar = document.getElementById(id);
-    let width = 0;
-
-    let interval = setInterval(() => {
-      if (width == level - 1) {
-        clearInterval(interval);
-      }
-
-      width += 1;
-      bar.style.width = width + "%";
-    }, 5);
-  }
 
   showSkills(skill) {
     let button = document.getElementById(skill);
@@ -128,6 +60,13 @@ export class AppComponent implements OnInit {
       document.getElementById(this.skills).classList.remove(this.className);
       this.skills = skill;
       document.getElementById(skill).classList.add(this.className);
+    }
+
+    if(skill == "behavioral"){
+      this.isTechnical=false;
+    }
+    else{
+      this.isTechnical=true;
     }
   }
 }
