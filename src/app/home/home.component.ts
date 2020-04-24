8654ced363e3147588dd096eae1ca68f1f0a7db1
Rishabh <rishabhmalhotra091@gmail.com>
2020-04-24 13:27:23 +0530

    bug fixed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,11 +7,35 @@ import { Router } from "@angular/router";
   styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+
+  about;
+  skills;
+  contact;
+
   constructor(private router: Router) {}
 
   ngOnInit() {}
 
   ngAfterViewInit() {
+    this.about=document.getElementById('aboutId').offsetTop;
+    this.skills=document.getElementById('skillsId').offsetTop;
+    this.contact=document.getElementById('contactId').offsetTop;
+  }
+
+  scrollTo(item){
+    if(item == 'about'){
+      this.toItem(this.about);
+    }
+    else if(item == 'skills'){
+      this.toItem(this.skills);
+    }
+    else if(item == 'contact'){
+      this.toItem(this.contact);
+    }
+  }
+
+  toItem(value){
+    window.scrollTo(0,value);
   }
 
   toProjects() {
